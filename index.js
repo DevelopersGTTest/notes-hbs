b9cfe8b1d23082adec36c6e36460aaf09b8a5a77
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-05-13 21:48:25 -0600

    handlebars config done!

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+//Loads the handlebars module
+const handlebars = require('express-handlebars')
 const express = require('express')
 const app = express()
 
 const P = 3000;
- 
+
+//Sets our app to use the handlebars engine
+app.set('view engine', 'handlebars');
+
+//Sets handlebars configurations (we will go through them later on)
+app.engine('handlebars', handlebars({
+    layoutsDir: __dirname + './layouts',
+}));
+
+//Serves static files (we need it to import a css file)
+app.use(express.static('views'))
+
+app.get('/', (req, res) => {
+    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    res.render('main', {layout : 'index'});
+    });
+    
+
 app.listen(P, ()=>{
     console.log(`server listen on ${P}`)
 });
