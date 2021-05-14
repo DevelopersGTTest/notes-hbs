0f84cb238918c8f1682d92a3111d2b6be2697c22
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-05-13 21:53:21 -0600

    refactor config engine

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-//Loads the handlebars module
 const handlebars = require('express-handlebars')
 const express = require('express')
 const app = express()
 
 const P = 3000;
 
-//Sets our app to use the handlebars engine
-app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
 
-//Sets handlebars configurations (we will go through them later on)
-app.engine('handlebars', handlebars({
-    layoutsDir: __dirname + './layouts',
+//instead of app.engine('handlebars', handlebars({
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    //new configuration parameter
+    extname: 'hbs'
 }));
 
-//Serves static files (we need it to import a css file)
 app.use(express.static('views'))
 
 app.get('/', (req, res) => {
-    //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
+    /***
+     *  Serves the body of the page aka "main.handlebars" 
+     * to the container //aka "index.hbs"
+     * 
+     * */
     res.render('main', {layout : 'index'});
-    });
+});
     
 
 app.listen(P, ()=>{
